29c48fe05e1ddce1f1c47891ec5990ec02e88e68
PatriciaAbreu <PatriciaAbreu@macbook-pro-de-patricia.local>
2015-09-28 12:32:57 -0300

    Verificação dos números do jogo da piscina de bolinhas ok!

--- AppFinal/AppFinal/GameScene_Espaco.swift
+++ AppFinal/AppFinal/GameScene_Espaco.swift
@@ -13,6 +13,7 @@ import AVFoundation
 class GameScene_Espaco: SKScene {
     
     var background_bolas: SKSpriteNode!
+    var background: SKSpriteNode!
     
     var movie: MPMoviePlayerController?
     var audio: AVAudioPlayer!
@@ -22,10 +23,33 @@ class GameScene_Espaco: SKScene {
     var audioS: String = ""
 
     var personagem_Paula: SKSpriteNode!
+    var personagem_placa: SKSpriteNode!
     
     var chat1: SKSpriteNode!
+    var chat2: SKSpriteNode!
+    var chat3: SKSpriteNode!
     
     var seta_play1: SKSpriteNode!
+    var seta_play2: SKSpriteNode!
+    var seta_play3: SKSpriteNode!
+    
+    var bola1: SKSpriteNode!
+    var bola2: SKSpriteNode!
+    var bola3: SKSpriteNode!
+    var bola4: SKSpriteNode!
+    var bola5: SKSpriteNode!
+    
+    var num1: Numero!
+    var num2: Numero!
+    var num3: Numero!
+    var num4: Numero!
+    var num5: Numero!
+    var num6: Numero!
+    var num7: Numero!
+    var num8: Numero!
+    var num9: Numero!
+    var num10: Numero!
+    
     
     override func didMoveToView(view: SKView) {
         
@@ -39,12 +63,83 @@ class GameScene_Espaco: SKScene {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        let touch = touches.first
+        let touchLocation = touch!.locationInNode(self)
+        
+        let toque = self.nodeAtPoint(touchLocation)
+        
+        if toque.name == "seta_play1" {
+            
+            chat1.removeFromParent()
+            seta_play1.removeFromParent()
+            movie?.view.hidden = true
+            
+            chat2 = SKSpriteNode(imageNamed: "chat_verde_texto_2")
+            chat2.zPosition = 1
+            chat2.name = "chat_verde_2"
+            chat2.size = CGSize(width: 200, height: 200)
+            chat2.position = CGPoint(x: 730, y: 300)
+            
+            seta_play2 = SKSpriteNode(imageNamed: "seta_verde")
+            seta_play2.zPosition = 1
+            seta_play2.name = "seta_play2"
+            seta_play2.size = CGSize(width: 50, height: 50)
+            seta_play2.position = CGPoint(x: 740, y: 280)
+            
+            video = exercicio.getVideo(3, video: "video2")
+            audioS = exercicio.getAudio(3, audio: "audio2")
+            
+            playVideo(video, tipo: "m4v")
+            playAudio(audioS, tipo: "m4a")
+            
+            addChild(chat2)
+            addChild(seta_play2)
+            
+        }else if toque.name == "seta_play2" {
+            
+            chat2.removeFromParent()
+            seta_play2.removeFromParent()
+            movie!.view.hidden = true
+            
+            chat3 = SKSpriteNode(imageNamed: "chat_verde_texto_3")
+            chat3.zPosition = 1
+            chat3.name = "chat_verde_3"
+            chat3.size = CGSize(width: 200, height: 200)
+            chat3.position = CGPoint(x: 730, y: 300)
+            
+            seta_play3 = SKSpriteNode(imageNamed: "seta_verde")
+            seta_play3.zPosition = 1
+            seta_play3.name = "seta_play3"
+            seta_play3.size = CGSize(width: 50, height: 50)
+            seta_play3.position = CGPoint(x: 740, y: 280)
+            
+            video = exercicio.getVideo(3, video: "video3")
+            audioS = exercicio.getAudio(3, audio: "audio3")
+            
+            playVideo(video, tipo: "m4v")
+            playAudio(audioS, tipo: "m4a")
+            
+            addChild(chat3)
+            addChild(seta_play3)
+            
+        }else if toque.name == "seta_play3" {
+            
+            chat3.removeFromParent()
+            seta_play3.removeFromParent()
+            movie?.view.hidden = true
+            
+            personagem_Paula.runAction(SKAction.moveToX(1200, duration: 0.6))
+            
+            contarBolas(1)
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
     
+    // função que monta a scene
     func montarScene() {
         //posicionando os menus na tela
         background_bolas = SKSpriteNode(imageNamed: "fundo_bolas")
@@ -70,7 +165,7 @@ class GameScene_Espaco: SKScene {
         //posiciona e adiciona a seta de próximo e a de voltar
         seta_play1 = SKSpriteNode(imageNamed: "seta_verde")
         seta_play1.zPosition = 1
-        seta_play1.name = "seta_play"
+        seta_play1.name = "seta_play1"
         seta_play1.size = CGSize(width: 50, height: 50)
         seta_play1.position = CGPoint(x: 740, y: 280)
         
@@ -78,6 +173,126 @@ class GameScene_Espaco: SKScene {
         addChild(personagem_Paula)
         addChild(chat1)
         addChild(seta_play1)
+    }
+    
+    //função que monta o exercicio
+    func contarBolas(personagem: Int) {
+        
+        if personagem == 1 {
+            
+            background = SKSpriteNode(imageNamed: "fundo_para_bolas")
+            background.zPosition = 1
+            background.name = "fundo"
+            background.size = CGSize(width: 500, height: 500)
+            background.position = CGPoint(x: 525, y: 400)
+            
+            personagem_placa = SKSpriteNode(imageNamed: "personagem_placa_1")
+            personagem_placa.zPosition = 1
+            personagem_placa.name = "personagem1"
+            personagem_placa.size = CGSize(width: 110, height: 305)
+            personagem_placa.position = CGPoint(x: 850, y: 150)
+            
+            bola1 = SKSpriteNode(imageNamed: "bola_amarela")
+            bola1.zPosition = 1
+            bola1.name = "bola1"
+            bola1.size = CGSize(width: 100, height: 100)
+            bola1.position = CGPoint(x: 440, y: 300)
+            
+            bola2 = SKSpriteNode(imageNamed: "bola_azul_claro")
+            bola2.zPosition = 1
+            bola2.name = "bola2"
+            bola2.size = CGSize(width: 100, height: 100)
+            bola2.position = CGPoint(x: 440, y: 470)
+            
+            bola3 = SKSpriteNode(imageNamed: "bola_rosa")
+            bola3.zPosition = 1
+            bola3.name = "bola3"
+            bola3.size = CGSize(width: 100, height: 100)
+            bola3.position = CGPoint(x: 640, y: 300)
+            
+            bola4 = SKSpriteNode(imageNamed: "bola_vermelha")
+            bola4.zPosition = 1
+            bola4.name = "bola4"
+            bola4.size = CGSize(width: 100, height: 100)
+            bola4.position = CGPoint(x: 640, y: 470)
+            
+            addChild(background)
+            addChild(personagem_placa)
+            addChild(bola1)
+            addChild(bola2)
+            addChild(bola3)
+            addChild(bola4)
+            
+        }
+        
+        posicionaNumeros()
+    }
+    
+    //função que posiciona os numeros
+    func posicionaNumeros(){
+    
+        num1 = Numero(texture: SKTexture(imageNamed: "num1"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 1)
+        num1.zPosition = 1
+        num1.name = "exercicio3_1"
+        num1.position = CGPoint(x: 600, y: 700)
+        
+        num2 = Numero(texture: SKTexture(imageNamed: "num2"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 2)
+        num2.zPosition = 1
+        num2.name = "exercicio3_2"
+        num2.position = CGPoint(x: 200, y: 500)
+        
+        num3 = Numero(texture: SKTexture(imageNamed: "num3"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 3)
+        num3.zPosition = 1
+        num3.name = "exercicio3_3"
+        num3.position = CGPoint(x: 150, y: 350)
+        
+        num4 = Numero(texture: SKTexture(imageNamed: "num4"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 4)
+        num4.zPosition = 1
+        num4.name = "exercicio3_4_certo"
+        num4.position = CGPoint(x: 100, y: 200)
+        
+        num5 = Numero(texture: SKTexture(imageNamed: "num5"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 5)
+        num5.zPosition = 1
+        num5.name = "exercicio3_5"
+        num5.position = CGPoint(x: 800, y: 550)
+        
+        num6 = Numero(texture: SKTexture(imageNamed: "num6"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 6)
+        num6.zPosition = 1
+        num6.name = "exercicio3_6"
+        num6.position = CGPoint(x: 200, y: 100)
+        
+        num7 = Numero(texture: SKTexture(imageNamed: "num7"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 7)
+        num7.zPosition = 1
+        num7.name = "exercicio3_7"
+        num7.position = CGPoint(x: 300, y: 700)
+        
+        num8 = Numero(texture: SKTexture(imageNamed: "num8"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 8)
+        num8.zPosition = 1
+        num8.name = "exercicio3_8"
+        num8.position = CGPoint(x: 850, y: 400)
+        
+        num9 = Numero(texture: SKTexture(imageNamed: "num9"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 9)
+        num9.zPosition = 1
+        num9.name = "exercicio3_9"
+        num9.position = CGPoint(x: 100, y: 650)
+        
+        num10 = Numero(texture: SKTexture(imageNamed: "num10"), color: UIColor.clearColor(), size: CGSize(width: 70, height: 70), numero: 10)
+        num10.zPosition = 1
+        num10.name = "exercicio3_10"
+        num10.position = CGPoint(x: 900, y: 700)
+        
+        
+        addChild(num1)
+        addChild(num2)
+        addChild(num3)
+        addChild(num4)
+        addChild(num5)
+        addChild(num6)
+        addChild(num7)
+        addChild(num8)
+        addChild(num9)
+        addChild(num10)
+
     }
     
     //função para tocar video
--- AppFinal/Numero.swift
+++ AppFinal/Numero.swift
@@ -198,5 +198,20 @@ class Numero: SKSpriteNode {
             (toque.parent as! GameScene_Operacoes).contouErrado()
         }
         
+        //faz a analise dos números do jogo da piscina de bolinhas
+        if toque.name == "exercicio3_4_certo" {
+            if toque.position.x >= 830 && toque.position.x <= 870 && toque.position.y >= 230 && toque.position.y <= 270 {
+                    print("PISCINAAA")
+                
+            }else {
+                
+                toque.position = Numero.toque
+            }
+            
+        }else if toque.name == "exercicio3_1" || toque.name == "exercicio3_2" || toque.name == "exercicio3_3" || toque.name == "exercicio3_4" || toque.name == "exercicio3_5" || toque.name == "exercicio3_6" || toque.name == "exercicio3_7" || toque.name == "exercicio3_8" || toque.name == "exercicio3_9" || toque.name == "exercicio3_10" {
+            
+            toque.position = Numero.toque
+        }
+        
     }
 }
